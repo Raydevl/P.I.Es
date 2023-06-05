139e2931eed568d1cced23ad9db26d0bfec21c4b
Raymond Morales <94730761+Raydevl@users.noreply.github.com>
2023-06-04 21:23:07 -0400

    imported PIEs from Repl
    imported main sourcecode my other acc as 'infinte-elevator' on Replit

---
+++ main.py
@@ -0,0 +1,41 @@
+import os
+import time
+import random
+import colorama 
+from colorama import init, Fore, Back, Style
+##everything after this defines the elevator 'story' generation, this similar to how 'Briery' (another project of mine) does it, but I'm using functions instead.
+def elewait(): #defines a elewait function, this is the elevator slowly going down the several floors before arriving at one which will be defined by 'elefloor' which will lead to rooms which will then be defined by a 'eleroom' function
+  colwall = random.choice(["white", "dark red", "black", "grey", "dark blue", "dark orange"]) #wall color
+  matwall = random.choice(["wooden", "concrete", "stainless steel", "stone"]) #material of the wall
+  music = random.choice(["C148's 'Subwoofer Lullaby'", "Frank Sinatra's 'My Way'", "Pink Floyd's 'The Gig in the sky'", "Soothing Elevator Music", "War's 'Why can't we be friends?"]) #music that plays (lol)
+  volmusic = random.choice(["Loud", "Soft"])
+  luxcolor = random.choice(["soft yellow", "soft white",])
+  luxbright = random.choice(["dimly lit", "brightly lit", "extremely bright"])
+  roomstatus = random.choice(["has reached a floor", "hasn't reached a floor yet"]) #status of weather the elevator is on a floor or not, very important later.
+  elewaitmgen = f"You are standing in a {colwall} colored {matwall} Elevator which {roomstatus}, the elevator lights are {luxbright} with a {luxcolor} shade. the elevator's speakers are playing {music} at a {volmusic} Volume."
+  print (elewaitmgen)
+  if roomstatus == "has reached a floor":
+    roominput = input("Do you walk out the elevator doors? y/n ")
+    if roominput.lower() == 'n':
+      elewait()
+    elif roominput.lower() == 'y':
+      print("I'll get back to this")
+    else:
+      print("Invalid input. Please enter 'y' or 'n'.")
+  time.sleep(2.75) #waits 2.75 seconds before generating next 'elewaitmgen' but only if roomstatus is in the 'hasn't reached a floor yet' state.
+init()
+def elefloor(): #defines the 'floor' which wil be generated when you say 'y'
+  matflwall = random.choice([])
+  colfl = random.choice(["grey", "black", "yellow", "white"])
+  musicfl = random.choice(["Resonance by home", "Around the world by Daft Punk", "Fortunate Son by CCR", "Gymnopedie by Erik Saite", "Generic Lobby Music", "Dream On By Aerosmith", "Going Down by Jake Chudnow", "It's My Life By The Animals", "Heartaches By Al Bowly", "Less Than by Nine Inch Nails"])
+  luxcolfl = random.choice(["White", "Yellow", "Soft Yellow", "Dark Blue", "Soft Blue", "Dark Red", "Soft Red"])
+  luxbrightfl = random.choice(["Dimly Lit", "Brightly Lit", "Averagly Lit"])
+  
+print(Back.BLACK + "A INFINITE ELEVATOR GAME" + Back.RESET)
+print(Back.RED + "CREATED BY rayDEVL" + Back.RESET)
+print(Back.BLACK + "STARTED ON 6/4/23" + Back.RESET)
+print(Back.RED + "YOUR GAME WILL START IN 5 SECONDS" + Back.RESET)
+print(Back.BLACK + "-----------------------" + Back.RESET)
+time.sleep(5)
+while True:
+  elewait()
---
+++ readme.md
@@ -0,0 +1,38 @@
+***A Infinite Elevator Generator***
+*Have you ever played The Infinite Elevator on roblox?*
+**this project is that but in python**
+note: I am horrible at coding and have learned coding over the past week or two, so don't get mad that a person whom lost 7/8th of their brain could code better than me
+Features:
+ -""procedural generation"" (not really, or someting, I don't know)
+ - infinite playability (not really but ok)
+ - procedural floor and room generation (I already said it but ***ok***)
+ - that's about it (wow this is shitty)
+planned features:
+- NPC's
+- Graphics {2d at first, 3d *maybe*} (this is probably not gonna happen)
+- greater story generation
+- Greater Dialogue
+- better re-playability
+- actual music (really bro???)
+credits:
+Lead Dev - Me
+Thoughts and Prayers - Me
+Coke Dealer - Jamal *(thank you for the 8 kg man)*
+Crystal Dealer - Janette *(thanks fr fr)*
+-----------------------------------
+that's it!
+Contact me:
+Discord: Raymond#7311
+Email (i don't check this at all mostly): raymondmorales098+infiniteelevator@gmail.com
+***FAQ***
+Q: Where is Jamal?
+A: Camden, NJ
+Q: Say my name.
+A: Fuck you
+Q: Google en passant
+A: new response just dropped
+Q: actual zombie
+..
+Q: why did my father leave me?
+A: You.
+that's it lol
